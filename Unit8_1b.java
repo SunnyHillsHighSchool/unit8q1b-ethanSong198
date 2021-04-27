8a8a9f518fdbb3d34934b2852c27d996872ec0e7
Ethan Song <>
2021-04-27 21:11:58 +0000

    Copied and pasted my response from Unit 8 FRQ

--- Unit8_1b.java
+++ Unit8_1b.java
@@ -2,6 +2,13 @@ public class Unit8_1a
 {
   public int [] [] modifyRows (int [] [] array)
  { 
+   for(int r = 0; r < array.length; r++){
+     for(int c = 0; c < array[0].length; c++){
+          if(r == 1 ||r % 2 == 1){
+               array[r][c] = array [r-1][c];
+          }
+     }
+}
 return array;
       }
     }
